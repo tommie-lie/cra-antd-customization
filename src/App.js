88b11129334aaf0d89e024a653593586341fd9c8
Thomas Liebetraut <thomas@tommie-lie.de>
2021-06-29 23:32:58 +0200

    use less-loader with inline syntax to customize antd's Less styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import "antd/dist/antd.css";
-import { Button, Typography } from "antd";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import "!style-loader!css-loader!less-loader?{'lessOptions':{'javascriptEnabled':true}}!./antd-overrides.less";
+import { Button, Input, Space, Typography } from "antd";
 
 function App() {
   return (
     <>
       <Typography.Title>Customize antd without hassle</Typography.Title>
-      <Button type="primary">This is a button</Button>
+      <div style={{ marginLeft: "2em" }}>
+        <Space direction="vertical">
+          <Input style={{ width: "250px" }} />
+          <Button type="primary">This is a button</Button>
+        </Space>
+      </div>
     </>
   );
 }
